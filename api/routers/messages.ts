99a4ce461c14c353c9f897a39042992fe9ad80d7
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-24 22:52:04 +0600

    Added imageUpload in messagesRouter.post

--- api/routers/messages.ts
+++ api/routers/messages.ts
@@ -1,13 +1,14 @@
 import {Router} from 'express';
 import fileDb from "../fileDb";
 import {MessageWithoutId} from "../types";
+import {imagesUpload} from "../multer";
 const messagesRouter = Router();
 messagesRouter.get('/', async (req, res)=>{
     const messages = await fileDb.getItems();
     res.send(messages);
 });
 
-messagesRouter.post('/', async(req, res)=>{
+messagesRouter.post('/', imagesUpload.single('image'), async(req, res)=>{
 
     const message: MessageWithoutId = {
         author: req.body.author,
