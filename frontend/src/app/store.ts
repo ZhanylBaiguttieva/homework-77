61a8296a03554b511c7c8a7b464e43c1cc4b00eb
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-24 23:35:48 +0600

    Added reducer

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,7 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {messagesReducer} from '../containers/messagesSlice.ts';
 
 export const  store = configureStore({
   reducer: {
+    messages: messagesReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
