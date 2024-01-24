313687680c0544c2d8ddff513b6d6b191bd9171c
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-24 23:11:50 +0600

    Added type Message and MessageMutation

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,12 @@
+export interface Message {
+  id: string,
+  author: string | null,
+  title: string,
+  image: string | null
+}
+
+export interface MessageMutation {
+  author: string | null,
+  title: string;
+  image: File | null;
+}
