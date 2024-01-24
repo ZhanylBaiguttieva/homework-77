ce2b78848c698cb3e6fd0e4ebb6963bd74878eda
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-25 00:47:58 +0600

    Added style MUI in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
+import MessageForm from './components/Form/MessageForm.tsx';
+import Messages from './components/Messages.tsx';
+import {Grid, Typography} from '@mui/material';
 
 
 function App() {
 
   return (
-    <>
-      Hello
-    </>
+    <Grid container direction="column" spacing={2}>
+      <Grid item>
+        <Typography variant="h4">Messages</Typography>
+      </Grid>
+      <Grid item>
+        <MessageForm/>
+      </Grid>
+      <Grid item container spacing={1}>
+        <Messages/>
+      </Grid>
+    </Grid>
   )
 }
 
